affb7c7cbf6d2d49bbed402105f21eecd0ec22a0
HQMIS <qiminis0801@gamil.com>
2013-06-24 02:26:52 +0800

    add table diy to record user's visit of website && code to realize this

--- itgps/src/com/itgps/controller/UserController.java
+++ itgps/src/com/itgps/controller/UserController.java
@@ -36,7 +36,7 @@ public class UserController {
 			username = user.getUsername();
 			System.out.print("Index: " + username + "\n");
 		}
-		
+
 		WebinfoImpl impl = new WebinfoImpl();
 		// impl.showInfo();
 
@@ -47,17 +47,18 @@ public class UserController {
 		model.addAttribute("bitcoinList", impl.showInfo("0", "2"));
 		return "/SlideVersion/index_slide.jsp";
 	}
-	
+
 	@RequestMapping(value = "/index_slide", method = RequestMethod.GET)
-	public String showIndexSlide(Model model, @ModelAttribute("user") User user,
-			SessionStatus status, HttpSession session) {
+	public String showIndexSlide(Model model,
+			@ModelAttribute("user") User user, SessionStatus status,
+			HttpSession session) {
 		String username = null;
 		if (session.getAttribute("currentUser") != null) {
 			user = (User) session.getAttribute("currentUser");
 			username = user.getUsername();
 			System.out.print("Index_Slide: " + username + "\n");
 		}
-		
+
 		WebinfoImpl impl = new WebinfoImpl();
 		// impl.showInfo();
 
@@ -68,7 +69,7 @@ public class UserController {
 		model.addAttribute("bitcoinList", impl.showInfo("0", "2"));
 		return "/SlideVersion/index_slide.jsp";
 	}
-	
+
 	@RequestMapping(value = "/itgps_slide", method = RequestMethod.GET)
 	public String showItgpsSlide(Model model) {
 		WebinfoImpl impl = new WebinfoImpl();
@@ -90,7 +91,7 @@ public class UserController {
 		model.addAttribute("deploySiteList", impl.showInfo("1", "12"));
 		return "/SlideVersion/itgps_slide.jsp";
 	}
-	
+
 	@RequestMapping(value = "/fegps_slide", method = RequestMethod.GET)
 	public String showFegpsSlide(Model model) {
 		WebinfoImpl impl = new WebinfoImpl();
@@ -103,7 +104,7 @@ public class UserController {
 		model.addAttribute("resCollectList", impl.showInfo("2", "3"));
 		return "/SlideVersion/fegps_slide.jsp";
 	}
-	
+
 	@RequestMapping(value = "/plgps_slide", method = RequestMethod.GET)
 	public String showPlgpsSlide(Model model) {
 		WebinfoImpl impl = new WebinfoImpl();
@@ -115,21 +116,18 @@ public class UserController {
 		model.addAttribute("haskellList", impl.showInfo("3", "2"));
 		return "/SlideVersion/plgps_slide.jsp";
 	}
-	
-	
-	
-	
-	
+
 	@RequestMapping(value = "/index_scroll", method = RequestMethod.GET)
-	public String showIndexScoll(Model model, @ModelAttribute("user") User user,
-			SessionStatus status, HttpSession session) {
+	public String showIndexScoll(Model model,
+			@ModelAttribute("user") User user, SessionStatus status,
+			HttpSession session) {
 		String username = null;
 		if (session.getAttribute("currentUser") != null) {
 			user = (User) session.getAttribute("currentUser");
 			username = user.getUsername();
 			System.out.print("Index_Scroll: " + username + "\n");
 		}
-		
+
 		WebinfoImpl impl = new WebinfoImpl();
 		// impl.showInfo();
 
@@ -140,7 +138,7 @@ public class UserController {
 		model.addAttribute("bitcoinList", impl.showInfo("0", "2"));
 		return "/ScrollVersion/index_scroll.jsp";
 	}
-	
+
 	@RequestMapping(value = "/itgps_scroll", method = RequestMethod.GET)
 	public String showItgpsScroll(Model model) {
 		WebinfoImpl impl = new WebinfoImpl();
@@ -162,7 +160,7 @@ public class UserController {
 		model.addAttribute("deploySiteList", impl.showInfo("1", "12"));
 		return "/ScrollVersion/itgps_scroll.jsp";
 	}
-	
+
 	@RequestMapping(value = "/fegps_scroll", method = RequestMethod.GET)
 	public String showFegpsScroll(Model model) {
 		WebinfoImpl impl = new WebinfoImpl();
@@ -175,7 +173,7 @@ public class UserController {
 		model.addAttribute("resCollectList", impl.showInfo("2", "3"));
 		return "/ScrollVersion/fegps_scroll.jsp";
 	}
-	
+
 	@RequestMapping(value = "/plgps_scroll", method = RequestMethod.GET)
 	public String showPlgpsScroll(Model model) {
 		WebinfoImpl impl = new WebinfoImpl();
@@ -187,11 +185,7 @@ public class UserController {
 		model.addAttribute("haskellList", impl.showInfo("3", "2"));
 		return "/ScrollVersion/plgps_scroll.jsp";
 	}
-	
-	
-	
-	
-	
+
 	@RequestMapping(value = "/index_text", method = RequestMethod.GET)
 	public String showIndexText(Model model, @ModelAttribute("user") User user,
 			SessionStatus status, HttpSession session) {
@@ -201,7 +195,7 @@ public class UserController {
 			username = user.getUsername();
 			System.out.print("Index_Text: " + username + "\n");
 		}
-		
+
 		WebinfoImpl impl = new WebinfoImpl();
 		// impl.showInfo();
 
@@ -212,7 +206,7 @@ public class UserController {
 		model.addAttribute("bitcoinList", impl.showInfo("0", "2"));
 		return "/TextVersion/index_text.jsp";
 	}
-	
+
 	@RequestMapping(value = "/itgps_text", method = RequestMethod.GET)
 	public String showItgpsText(Model model) {
 		WebinfoImpl impl = new WebinfoImpl();
@@ -234,7 +228,7 @@ public class UserController {
 		model.addAttribute("deploySiteList", impl.showInfo("1", "12"));
 		return "/TextVersion/itgps_text.jsp";
 	}
-	
+
 	@RequestMapping(value = "/fegps_text", method = RequestMethod.GET)
 	public String showFegpsText(Model model) {
 		WebinfoImpl impl = new WebinfoImpl();
@@ -247,7 +241,7 @@ public class UserController {
 		model.addAttribute("resCollectList", impl.showInfo("2", "3"));
 		return "/TextVersion/fegps_text.jsp";
 	}
-	
+
 	@RequestMapping(value = "/plgps_text", method = RequestMethod.GET)
 	public String showPlgpsText(Model model) {
 		WebinfoImpl impl = new WebinfoImpl();
@@ -259,18 +253,7 @@ public class UserController {
 		model.addAttribute("haskellList", impl.showInfo("3", "2"));
 		return "/TextVersion/plgps_text.jsp";
 	}
-	
-	
-	
-	
-	
-	
-	
 
-	
-	
-	
-	
 	@RequestMapping(value = "/geek_slide", method = RequestMethod.POST)
 	public String home(Model model) {
 		// WebinfoImpl impl = new WebinfoImpl();
@@ -278,9 +261,6 @@ public class UserController {
 		return "/SlideVersion/index_slide.jsp";
 	}
 
-	
-	
-	
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String showLogin(Model model) {
 		model.addAttribute("user", new User());
@@ -293,7 +273,8 @@ public class UserController {
 	public String login(Model model, @ModelAttribute("user") User user,
 			SessionStatus status, HttpSession session) {
 		User userinfo = userService.findByUsername(user.getUsername());
-		if (userinfo == null || !userinfo.getPassword().equals(user.getPassword())) {
+		if (userinfo == null
+				|| !userinfo.getPassword().equals(user.getPassword())) {
 			model.addAttribute("login", "err");
 			return "jsp/sign/login.jsp";
 		}
@@ -324,7 +305,7 @@ public class UserController {
 		}
 		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		user.setRegisterTime(df.format(new Date()));
-		
+
 		userService.saveUser(user);
 		model.addAttribute("login", "suc");
 		session.setAttribute("currentUser", user);
@@ -343,30 +324,37 @@ public class UserController {
 		showIndex(model, user, status, session);
 		return "SlideVersion/index_slide.jsp";
 	}
-	
-	
-	
-	
-	
+
 	@RequestMapping(value = "/detail", method = RequestMethod.GET)
 	public String showDetail(String fc, String sc, Model model) {
 		System.out.print("FC: " + fc + "     &&     " + "SC: " + sc + "\n");
-		
+
 		WebinfoImpl impl = new WebinfoImpl();
 		model.addAttribute("detailList", impl.showInfo(fc, sc));
-		
+
 		return "/detail.jsp";
 	}
-	
+
 	@RequestMapping(value = "/clickUrl", method = RequestMethod.GET)
-	public String clickUrl(String url, Model model, @ModelAttribute("user") User user,
-			SessionStatus status, HttpSession session) {
+	public String clickUrl(String url, Model model,
+			@ModelAttribute("user") User user, SessionStatus status,
+			HttpSession session) {
 		System.out.print("URL: " + url + "\n");
-		
+
 		WebinfoImpl impl = new WebinfoImpl();
 		impl.countAdd(url);
-		//showIndex(model, user, status, session);
-		//return "redirect:http://127.0.0.1:8080/itgps/itgps_slide#/online_learn";
+
+		String username = null;
+		if (session.getAttribute("currentUser") != null) {
+			user = (User) session.getAttribute("currentUser");
+			username = user.getUsername();
+			System.out.print("Click Url: " + username + "\n");
+			impl.insertOrUpdate(username, url);
+		}
+
+		// showIndex(model, user, status, session);
+		// return
+		// "redirect:http://127.0.0.1:8080/itgps/itgps_slide#/online_learn";
 		return "redirect:" + url;
 	}
 
--- itgps/src/com/itgps/dao/impl/WebinfoImpl.java
+++ itgps/src/com/itgps/dao/impl/WebinfoImpl.java
@@ -2,6 +2,7 @@ package com.itgps.dao.impl;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,13 +16,14 @@ import com.itgps.entity.Webinfo;
 
 @Repository("Webinfo")
 public class WebinfoImpl implements WebinfoDao {
-	private static Logger _logger=Logger.getLogger(WebinfoImpl.class);
+	private static Logger _logger = Logger.getLogger(WebinfoImpl.class);
+
 	@Override
 	public List<Webinfo> showInfo(String fc, String sc) {
 		// TODO Auto-generated method stub
 		ArrayList<Webinfo> webinfoList = new ArrayList<Webinfo>();
 		Webinfo info = null;
-		String sql=" select * from webinfo where fc = ? and sc = ? order by count desc";
+		String sql = "select * from webinfo where fc = ? and sc = ? order by count desc";
 		SqlUtil help = new SqlUtil();
 		PreparedStatement ps = help.prepareStatement(sql);
 		ResultSet rs;
@@ -29,47 +31,49 @@ public class WebinfoImpl implements WebinfoDao {
 			ps.setString(1, fc);
 			ps.setString(2, sc);
 			rs = help.ExecuteResultSet();
-			while( rs.next() ){
-				info = new Webinfo(rs.getString("url"), rs.getString("name"), rs.getString("logo"), rs.getString("title"));
+			while (rs.next()) {
+				info = new Webinfo(rs.getString("url"), rs.getString("name"),
+						rs.getString("logo"), rs.getString("title"));
 				webinfoList.add(info);
 			}
 			rs.close();
 		} catch (Exception e) {
 			_logger.error("执行showInfo方法出错:" + e.getMessage());
 			e.printStackTrace();
-		} finally { 
+		} finally {
 			help.ClosePrepareStatement();
 		}
 		return webinfoList;
 	}
-	
+
 	public List<Webinfo> top20() {
 		// TODO Auto-generated method stub
 		ArrayList<Webinfo> webinfoList = new ArrayList<Webinfo>();
 		Webinfo info = null;
-		String sql=" select * from webinfo order by count desc limit 20";
+		String sql = "select * from webinfo order by count desc limit 20";
 		SqlUtil help = new SqlUtil();
 		PreparedStatement ps = help.prepareStatement(sql);
 		ResultSet rs;
 		try {
 			rs = help.ExecuteResultSet();
-			while( rs.next() ){
-				info = new Webinfo(rs.getString("url"), rs.getString("name"), rs.getString("logo"), rs.getString("title"));
+			while (rs.next()) {
+				info = new Webinfo(rs.getString("url"), rs.getString("name"),
+						rs.getString("logo"), rs.getString("title"));
 				webinfoList.add(info);
 			}
 			rs.close();
 		} catch (Exception e) {
 			_logger.error("执行top20方法出错:" + e.getMessage());
 			e.printStackTrace();
-		} finally { 
+		} finally {
 			help.ClosePrepareStatement();
 		}
 		return webinfoList;
 	}
-	
+
 	public void countAdd(String url) {
 		// TODO Auto-generated method stub
-		String sql="update webinfo set count = count + 1 where url = ?";
+		String sql = "update webinfo set count = count + 1 where url = ?";
 		SqlUtil help = new SqlUtil();
 		PreparedStatement ps = help.prepareStatement(sql);
 		try {
@@ -78,23 +82,60 @@ public class WebinfoImpl implements WebinfoDao {
 		} catch (Exception e) {
 			_logger.error("执行countAdd方法出错:" + e.getMessage());
 			e.printStackTrace();
-		} finally { 
+		} finally {
 			help.ClosePrepareStatement();
 		}
 	}
-	
+
+	public void insertOrUpdate(String username, String url) {
+		// TODO Auto-generated method stub
+		String sql = "update diy set count = count + 1 where username = ? and url = ?";
+		SqlUtil help = new SqlUtil();
+		PreparedStatement ps = help.prepareStatement(sql);
+		try {
+			ps.setString(1, username);
+			ps.setString(2, url);
+			int result = help.ExecuteNonQuery();
+			System.out.print(result + "\n");
+			System.out.print("Username && Url: " + username + " && " + url
+					+ "\n");
+
+			if (0 == result) {
+				sql = "insert into diy(username, url, count) values (?, ?, ?)";
+				ps = help.prepareStatement(sql, true);
+				try {
+					ps.setString(1, username);
+					ps.setString(2, url);
+					ps.setInt(3, 1);
+					help.InsertAndGetNewId();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					_logger.error("执行insertOrUpdate-Insert方法出错:"
+							+ e1.getMessage());
+					e1.printStackTrace();
+				}
+			}
+
+		} catch (Exception e) {
+			_logger.error("执行insertOrUpdate-Update方法出错:" + e.getMessage());
+			e.printStackTrace();
+		} finally {
+			help.ClosePrepareStatement();
+		}
+	}
+
 	@Override
 	public void saveOrUpdate(Webinfo e) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	@Override
 	public void delete(Webinfo e) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	@Override
 	public List<Webinfo> findByCriteria(Criterion criterion) {
 		// TODO Auto-generated method stub
